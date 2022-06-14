ddc4ccc378b39bf7e66916acaa7903e022717389
maisahr <ma.isahr7@gmail.com>
2022-06-14 09:53:37 -0700

    Creates CapsulesStatus component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
+import { CapsulesStatus } from './components/CapsulesStatus/CapsulesStatus';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <CapsulesStatus></CapsulesStatus>
       <Footer></Footer>
     </>
   );
---
+++ src/components/CapsulesStatus/CapsulesStatus.js
@@ -0,0 +1,28 @@
+import './CapsulesStatus.css'
+import active from '../../assets/rocketActive.svg'
+import destroyed from '../../assets/rocketDeactivate.svg'
+import unknown from '../../assets/rocketUnknown.svg'
+
+export const CapsulesStatus = () => {
+    return(
+        <section>
+            <h2>Capsules status:</h2>
+            <div>
+                <figure id='active'>
+                    <figcaption>Active</figcaption>
+                    <img src={active} alt='active rocket'></img>
+                </figure>
+
+                <figure id='destroyed'>
+                    <figcaption>Destroyed</figcaption>
+                    <img src={destroyed} alt='destroyed rocket'></img>
+                </figure>
+
+                <figure id='unknown'>
+                    <figcaption>Unknown</figcaption>
+                    <img src={unknown} alt='unknown rocket'></img>
+                </figure>
+            </div>
+        </section>
+    );
+}
