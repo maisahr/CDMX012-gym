0076b5cb002143710f52ccad2d948711eeacc627
maisahr <ma.isahr7@gmail.com>
2022-07-13 11:11:06 -0700

    Final design details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import './Footer.css'
 export const Footer = () => {
     return(
         <footer>
-            <p>FIX YOUR LITTLE PROBLEM AND LIGHT THIS CANDLE</p>
+            <p>FIX YOUR LITTLE PROBLEM AND LIGHT THIS CANDLE.</p>
         </footer>
     );
 }
